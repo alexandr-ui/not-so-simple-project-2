6138a73051217cb771c19dc4d138aeec33b3b7eb
OleksandrB_QA <Oleksandr.Barylovskyi.ext@compleet.com>
2021-12-02 19:11:42 +0200

    Dec 1 II

--- src/test/java/TextBoxTest.java
+++ src/test/java/TextBoxTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,20 +14,73 @@ public class TextBoxTest {
         Configuration.startMaximized = true;
     }
 
+//    @Test
+//    void successfulSubmitForm() {
+//        open("https://demoqa.com/text-box");
+//
+//        $("[id=userName]").setValue("Some user");
+//        $("[id=userEmail]").setValue("Some@email.com");
+//        $("[id=currentAddress]").setValue("Some address");
+//        $("[id=permanentAddress]").setValue("Some permanent address");
+//        $("[id=submit]").click();
+//
+//        $("[id=name]").shouldHave(text("Name:"), text("Some user"));
+//        $("[id=email]").shouldHave(text("Some@email.com"));
+//        $("p[id=currentAddress]").shouldHave(text("Some address"));
+//        $("p[id=permanentAddress]").shouldHave(text("Some permanent address"));
+//
+//    }
+
+//    @Test
+//    void successfulSubmitFormWithVariable() {
+//        String userName = "some user";
+//
+//        open("https://demoqa.com/text-box");
+//
+//        $("[id=userName]").setValue(userName);
+//        $("[id=userEmail]").setValue("Some@email.com");
+//        $("[id=currentAddress]").setValue("Some address");
+//        $("[id=permanentAddress]").setValue("Some permanent address");
+//        $("[id=submit]").click();
+//
+//        $("[id=name]").shouldHave(text("Name:"), text(userName));
+//        $("[id=email]").shouldHave(text("Some@email.com"));
+//        $("p[id=currentAddress]").shouldHave(text("Some address"));
+//        $("p[id=permanentAddress]").shouldHave(text("Some permanent address"));
+//
+//    }
+
+//    @Test
+//    void successfulSubmitFormSearchInOutput() {
+//        String userName = "some user";
+//
+//        open("https://demoqa.com/text-box");
+//
+//        $("[id=userName]").setValue(userName);
+//        $("[id=userEmail]").setValue("Some@email.com");
+//        $("[id=currentAddress]").setValue("Some address");
+//        $("[id=permanentAddress]").setValue("Some permanent address");
+//        $("[id=submit]").click();
+//
+//        $("[id=output]").shouldHave((text(userName)), text("Some@email.com"), text("Some address"),
+//                text("Some permanent address"));
+//
+//    }
+
     @Test
-    void successfulSubmitForm() {
+    void successfulSubmitFormWithBadLocators() {
+        String userName = "some user";
+
         open("https://demoqa.com/text-box");
 
-        $("[id=userName]").setValue("Some user");
-        $("[id=userEmail]").setValue("Some@email.com");
-        $("[id=currentAddress]").setValue("Some address");
-        $("[id=permanentAddress]").setValue("Some permanent address");
-        $("[id=submit]").click();
+        $("[id=userName]").setValue(userName);
+        $("[placeholder=\"name@example.com\"]").setValue("Some@email.com");
+        $("textarea").setValue("Some address");
+        $(".form-control", 3).setValue("Some permanent address");
+        $(byText("Submit")).click();
 
-        $("[id=name]").shouldHave(text("Name:"), text("Some user"));
-        $("[id=email]").shouldHave(text("Some@email.com"));
-        $("p[id=currentAddress]").shouldHave(text("Some address"));
-        $("p[id=permanentAddress]").shouldHave(text("Some permanent address"));
+        $("[id=output]").shouldHave((text(userName)), text("Some@email.com"), text("Some address"),
+                text("Some permanent address"));
 
     }
 }
